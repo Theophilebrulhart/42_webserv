ad7ea9fe3d9b997e29d3dd9eb8d400b3958b0f19
Theo <tbrulhar@student.42lausanne.ch>
2023-06-30 17:17:51 +0200

    cleaning testServer.cpp

--- Network/Server/TestServer.cpp
+++ Network/Server/TestServer.cpp
@@ -6,7 +6,7 @@
 /*   By: tbrulhar <tbrulhar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 13:57:16 by tbrulhar          #+#    #+#             */
-/*   Updated: 2023/06/30 16:35:15 by tbrulhar         ###   ########.fr       */
+/*   Updated: 2023/06/30 17:17:02 by tbrulhar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -97,7 +97,7 @@ void SERVER::TestServer::launch()
     initialSocketPollfd.fd = initialSocket;
     initialSocketPollfd.events = POLLIN;
     initialSocketPollfd.revents = 0;
-    clientSockets.push_back(initialSocketPollfd); // Ajout du socket du serveur à la liste
+    clientSockets.push_back(initialSocketPollfd);
 
     std::cout << "Serveur en attente de connexions..." << std::endl;
 
@@ -131,10 +131,8 @@ void SERVER::TestServer::launch()
                 newClientPollfd.fd = newClientSocket;
                 newClientPollfd.events = POLLIN;
                 newClientPollfd.revents = 0;
-                clientSockets.push_back(newClientPollfd); // Ajout du nouveau client à la liste
+                clientSockets.push_back(newClientPollfd);
             }
-
-            // Ignorer les autres sockets dans cette itération
             continue;
         }
 
@@ -154,142 +152,23 @@ void SERVER::TestServer::launch()
                     }
                     close(clientSockets[i].fd);
                     clientSockets.erase(clientSockets.begin() + i);
-                    --i; // Réduire l'indice après la suppression d'un élément
+                    --i;
                 } else {
                     // Traiter les données reçues du client
                     std::cout << "Données reçues : " << buffer << std::endl;
                     _buffer = buffer;
 
-                    // Envoyer une réponse au client (exemple)
+                    // Envoyer une réponse au client
                     _handler(clientSockets[i].fd);
                     _responder(clientSockets[i].fd);
                 }
-
-                // Ignorer les autres sockets dans cette itération
                 break;
             }
         }
          std::cout << "\e[0;36m\n===== DONE =====\e[0m\n";
     }
-
-    // Fermer tous les sockets clients restants
     for (size_t i = 0; i < clientSockets.size(); ++i) {
         close(clientSockets[i].fd);
     }
-
-    // Fermer le socket du serveur
     close(initialSocket);
 }
-
-
-// void SERVER::TestServer::launch(void)
-// {
-//     std::cout << "\e[0;31m****STARTING*****\e[0m\n";
-//     int initialSocket = getServerSocket()->getSocketFd();
-//     std::cout << "\e[0;31minitialsocket " << initialSocket << "\e[0m\n\n";
-
-//     fd_set readfds, writefds;
-//     FD_ZERO(&readfds);
-//     FD_ZERO(&writefds);
-//     FD_SET(initialSocket, &readfds);
-//     int maxfd = initialSocket;
-
-//     while (true)
-//     {
-//         std::cout << "\n\e[0;32m===== WAITING =====\e[0m\n";
-//         fd_set tempReadFds = readfds;
-//         fd_set tempWriteFds = writefds;
-
-//         // Utilisation de select() pour attendre des événements sur les sockets
-//         int activity = select(maxfd + 1, &tempReadFds, &tempWriteFds, NULL, NULL);
-
-//         if (activity == -1)
-//         {
-//             std::perror("Erreur lors de l'appel à select().");
-//             close(initialSocket);
-//             return;
-//         }
-
-//         // Mettre à jour l'ensemble writefds
-//         writefds = tempWriteFds;
-
-//         // Nouvelle connexion entrante
-//         if (FD_ISSET(initialSocket, &tempReadFds))
-//         {
-//             // Accepter la nouvelle connexion
-//             int clientSocket = accept(initialSocket, NULL, NULL);
-//             std::cout << "\e[0;31mnouvelle connexion : " << clientSocket << "\e[0m\n";
-//             if (clientSocket == -1)
-//             {
-//                 std::cerr << "Erreur lors de l'acceptation de la connexion." << std::endl;
-//                 close(initialSocket);
-//                 return;
-//             }
-
-//             // Ajouter le socket client à la liste
-//             _clientSockets.push_back(clientSocket);
-//             std::cout << "Nouvelle connexion entrante. Client ID: " << clientSocket << std::endl;
-
-//             // Mettre à jour le descripteur de fichier maximal
-//             if (clientSocket > maxfd)
-//                 maxfd = clientSocket;
-
-//             // Ajouter le socket client à l'ensemble des descripteurs de fichiers en lecture et en écriture
-//             FD_SET(clientSocket, &readfds);
-//             FD_SET(clientSocket, &writefds);
-//         }
-
-//         // Vérifier les sockets des clients existants pour les données entrantes et sortantes
-//         for (size_t i = 0; i < _clientSockets.size(); ++i)
-//         {
-//             int clientSocket = _clientSockets[i];
-
-//             if (FD_ISSET(clientSocket, &tempReadFds))
-//             {
-//                 char buffer[4096];
-//                 memset(buffer, 0, sizeof(buffer));
-
-//                 // Lire les données reçues
-//                 ssize_t bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
-//                 if (bytesRead == -1)
-//                 {
-//                     std::cerr << "Erreur lors de la réception des données." << std::endl;
-//                     close(initialSocket);
-//                     return;
-//                 }
-
-//                 if (bytesRead == 0)
-//                 {
-//                     // Le client a fermé la connexion
-//                     std::cout << "Connexion fermée. Client ID: " << clientSocket << std::endl;
-//                     close(clientSocket);
-//                     FD_CLR(clientSocket, &readfds);
-//                     FD_CLR(clientSocket, &writefds);
-//                     _clientSockets.erase(_clientSockets.begin() + i);
-//                     --i;
-//                 }
-//                 else
-//                 {
-//                     // Afficher les informations du socket client
-//                     std::cout << "Client ID: " << clientSocket << std::endl;
-
-//                     // Faire quelque chose avec les données reçues
-//                     _buffer = buffer;
-//                     _handler(clientSocket);
-//                 }
-//             }
-
-//             if (FD_ISSET(clientSocket, &writefds))
-//             {
-//                 std::cout << "Socket prête à l'écriture. Client ID: " << clientSocket << std::endl;
-//                 _responder(clientSocket);
-//             }
-//         }
-
-//         std::cout << "\e[0;36m\n===== DONE =====\e[0m\n";
-//     }
-
-//     // Fermer le socket du serveur
-//     close(initialSocket);
-//     return;
-// }
